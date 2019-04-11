91383fa064ee2ff1f8d0ee43b33fc0288ee6dc43
Martin Haidn <martin.haidn@nic.at>
2019-04-11 17:45:14 +0200

    Reduced atEppWithdrawRequest as eppRequest adapter
    Added argument validation to adapter

--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppWithdrawRequest.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppWithdrawRequest.php
@@ -8,49 +8,33 @@
 
 namespace Metaregistrar\EPP;
 
-use atEppException;
-
 /**
- * Class atEppWithdrawRequest represents the at-registry specific withdraw request where the responsibility of
- * a domain is delegated back to nic.at
+ * Class atEppWithdrawRequest works only as adapter in order to map a atEppResponse.
  *
  * @package Metaregistrar\EPP
  */
 class atEppWithdrawRequest extends eppRequest
 {
-    use atEppCommandTrait;
 
-    function __construct(eppDomain $withdrawDomain, bool $zoneDeletion, atEppExtensionChain $atEppExtensionChain = null)
+
+    /**
+     * atEppWithdrawRequest constructor.
+     * @param $arguments
+     *
+     * @throws \atEppException
+     */
+    public function __construct($arguments)
     {
-        $this->atEppExtensionChain = $atEppExtensionChain;
+        $this->validateArguments($arguments);
         parent::__construct();
-
-        $this->setDomain($withdrawDomain, $zoneDeletion);
-        $this->setAtExtensions();
-        $this->addSessionId();
     }
 
-    function __destruct() {
-        parent::__destruct();
-    }
-
-    public function setDomain(eppDomain $domain, bool $zoneDeleteion) {
-        if (!strlen($domain->getDomainname())) {
-            throw new atEppException('atEppWithdrawRequest domain object does not contain a valid domain name');
+    protected function validateArguments($arguments)
+    {
+        if (! key_exists('domain_name', $arguments) || ! key_exists('zone_deletion', $arguments))
+        {
+            throw new \atEppException(
+                'atEppWithdrawRequest requires two arguments domain_name:string and zone_deletion:boolean.');
         }
-
-        /* Creating the withdraw object */
-        $this->domainobject = $this->createElement('withdraw');
-
-        $domainWithdraw = $this->createElement('domain:withdraw');
-        $domainWithdraw->setAttribute('xmlns:xsi', atEppConstants::namespaceAtExtDomain);
-        $domainWithdraw->setAttribute('xsi:schemaLocation', atEppConstants::schemaLocationAtExt);
-
-        $domainWithdraw->appendChild($this->createElement('domain:name', $domain->getDomainname()));
-        $domainWithdraw->appendChild($this->createElement('domain:zd')->setAttribute('value', $zoneDeleteion));
-
-        $this->domainobject->appendChild($domainWithdraw);
-        $this->getCommand()->appendChild($this->domainobject);
     }
-
 }
