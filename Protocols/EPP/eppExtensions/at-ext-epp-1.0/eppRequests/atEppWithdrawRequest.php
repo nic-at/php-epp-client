aff03f40c923da7c3d015644fd86d215851fc4a9
Martin Haidn <martin.haidn@nic.at>
2019-04-12 10:53:22 +0200

    atEppWithdrawRequest cleanup

--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppWithdrawRequest.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppWithdrawRequest.php
@@ -8,49 +8,12 @@
 
 namespace Metaregistrar\EPP;
 
-use atEppException;
-
 /**
- * Class atEppWithdrawRequest represents the at-registry specific withdraw request where the responsibility of
- * a domain is delegated back to nic.at
+ * Class atEppWithdrawRequest works only as adapter in order to map a atEppResponse.
  *
  * @package Metaregistrar\EPP
  */
 class atEppWithdrawRequest extends eppRequest
 {
-    use atEppCommandTrait;
-
-    function __construct(eppDomain $withdrawDomain, bool $zoneDeletion, atEppExtensionChain $atEppExtensionChain = null)
-    {
-        $this->atEppExtensionChain = $atEppExtensionChain;
-        parent::__construct();
-
-        $this->setDomain($withdrawDomain, $zoneDeletion);
-        $this->setAtExtensions();
-        $this->addSessionId();
-    }
-
-    function __destruct() {
-        parent::__destruct();
-    }
-
-    public function setDomain(eppDomain $domain, bool $zoneDeleteion) {
-        if (!strlen($domain->getDomainname())) {
-            throw new atEppException('atEppWithdrawRequest domain object does not contain a valid domain name');
-        }
-
-        /* Creating the withdraw object */
-        $this->domainobject = $this->createElement('withdraw');
-
-        $domainWithdraw = $this->createElement('domain:withdraw');
-        $domainWithdraw->setAttribute('xmlns:xsi', atEppConstants::namespaceAtExtDomain);
-        $domainWithdraw->setAttribute('xsi:schemaLocation', atEppConstants::schemaLocationAtExt);
-
-        $domainWithdraw->appendChild($this->createElement('domain:name', $domain->getDomainname()));
-        $domainWithdraw->appendChild($this->createElement('domain:zd')->setAttribute('value', $zoneDeleteion));
-
-        $this->domainobject->appendChild($domainWithdraw);
-        $this->getCommand()->appendChild($this->domainobject);
-    }
 
 }
